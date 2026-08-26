56bf125ec3eab8ab63966e8652965ecb5ec7c3be
Skuba60 <paoloantares@tiscali.it>
2026-08-26 12:47:49 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -45,7 +45,7 @@ HEADERS = {
     ),
     "Accept-Language": "sq,it;q=0.9,en;q=0.8",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
+    
     "Referer": "https://www.google.com/",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
